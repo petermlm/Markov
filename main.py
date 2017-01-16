ab70ef80eadd3b8149dd01ddfe8c4d4940694059
Pedro Melgueira <pedromelgueira@gmail.com>
2017-01-16 01:47:04 +0000

    Cosmetics

--- main.py
+++ main.py
@@ -4,92 +4,107 @@ import sys
 
 import numpy as np
 
+
+special_chars = [
+    ".",
+    ",",
+    ";",
+    "!",
+    "?",
+]
+
+ignored_chars = [
+    "\"",
+    "”",
+    "“",
+    "(",
+    ")"
+]
+
+
+def cleanLine(regex_special, regex_ignored, line):
+    line = line.strip()
+    line = regex_special.sub(" \g<0> ", line)
+    line = regex_ignored.sub(" ", line)
+    line = line.lower()
+    return line.split()
+
+
 def makeNetwork():
-    tokens_count = {}
-    tokens_index = []
+    tokens_word2index = {}
+    tokens_index2word = []
 
     # Read every word and build matrix
-    regex = re.compile("[.,;!?\"”“]")
+    regex_special = re.compile("[" + "".join(special_chars) + "]")
+    regex_ignored = re.compile("[" + "".join(ignored_chars) + "]")
 
     cindex = 0
-    with open("the_idiot") as f:
-        for i in f:
-            i_strip = i.strip()
-            # i_regex = regex.sub(" \g<0> ", i_strip)
-            i_regex = regex.sub(" ", i_strip)
-            i_low = i_regex.lower()
-            tokens = i_low.split()
-
-            for j in tokens:
-                if j not in tokens_count:
-                    tokens_count[j] = cindex
-                    tokens_index.append(j)
+    with open("the_idiot") as fd:
+        for line in fd:
+            for token in cleanLine(regex_special, regex_ignored, line):
+                if token not in tokens_word2index:
+                    tokens_word2index[token] = cindex
+                    tokens_index2word.append(token)
                     cindex += 1
 
     # Count
     network = []
-    for i in range(len(tokens_count)):
-        network.append(np.zeros(len(tokens_count)))
+    words_num = cindex
+    for i in range(words_num):
+        network.append(np.zeros(words_num))
 
     prev = ""
     cur = ""
+    first_step = True
 
-    step = 0
+    with open("the_idiot") as fd:
+        for line in fd:
+            for token in cleanLine(regex_special, regex_ignored, line):
+                if first_step:
+                    cur = token
+                    first_step = False
+                    continue
 
-    with open("the_idiot") as f:
-        for i in f:
-            i_strip = i.strip()
-            # i_regex = regex.sub(" \g<0> ", i_strip)
-            i_regex = regex.sub(" ", i_strip)
-            i_low = i_regex.lower()
-            tokens = i_low.split()
+                prev = cur
+                cur = token
 
-            for j in tokens:
-                if step == 0:
-                    prev = j
-                elif step == 1:
-                    cur = j
-                else:
-                    prev = cur
-                    cur = j
-                    network[tokens_count[prev]][tokens_count[cur]] += 1
+                prev_index = tokens_word2index[prev]
+                cur_index = tokens_word2index[cur]
 
-                step += 1
+                network[prev_index][cur_index] += 1
 
     # Normalize
     for i in range(len(network)):
         network[i] = network[i] / network[i].sum()
 
-    return network, tokens_index
+    return network, tokens_index2word
 
 
-def makeRandomText(network, tokens_index, words_num):
+def makeRandomText(network, tokens_index2word, words_num):
     c_index = random.randint(0, len(network)-1)
 
     for i in range(words_num):
-        sys.stdout.write(tokens_index[c_index] + " ")
+        # Write current word
+        c_word = tokens_index2word[c_index]
 
-        # prev = tokens_index[c_index]
-        # cur = tokens_index[c_index]
-        # while prev == cur:
-        #     r = random.random()
-        #     for jindex, j in enumerate(network[c_index]):
-        #         if r < j:
-        #             c_index = jindex
-        #             cur = tokens_index[c_index]
-        #             print(r)
-        #             print(prev, cur)
-        #             break
+        if i != 0 and c_word not in special_chars:
+            sys.stdout.write(" ")
 
-        r = random.random()
-        for jindex, j in enumerate(network[c_index]):
-            if r < j:
-                c_index = jindex
+        sys.stdout.write(c_word)
+
+        # Determine next index
+        rand_val = random.random()
+        next_prob_cum = 0
+        for next_index, next_prob in enumerate(network[c_index]):
+            next_prob_cum += next_prob
+
+            if rand_val < next_prob_cum:
+                c_index = next_index
                 break
 
 
 if __name__ == "__main__":
-    network, tokens_index = makeNetwork()
+    network, tokens_index2word = makeNetwork()
 
     # network = [
     #     np.array([0.05, 0.8, 0.15]),
@@ -97,6 +112,6 @@ if __name__ == "__main__":
     #     np.array([0.8, 0.15, 0.05])
     # ]
     #
-    # tokens_index = ["i", "am", "me"]
+    # tokens_index2word = ["i", "am", "me"]
 
-    makeRandomText(network, tokens_index, 1000)
+    makeRandomText(network, tokens_index2word, 100)
