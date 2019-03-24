ba81591f8660ece2e133ae6615da2e6c2946909d
Pedro Melgueira <pedromelgueira@gmail.com>
2019-03-24 19:14:53 +0100

    Adds setup.py and gitignore file

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='Markov',
+      version='1.0',
+      description='Markov Text Generation',
+      author='Pedro Melgueira',
+      py_modules=['markov'],
+)
