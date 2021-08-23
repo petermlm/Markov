74f573e197bca5c599136f61672605b570183fbc
Pedro Melgueira <pedromelgueira@gmail.com>
2021-08-23 20:03:44 +0200

    Fixes setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 from distutils.core import setup
 
 setup(name='Markov',
-      version='1.0',
+      version='1.2',
       description='Markov Text Generation',
       author='Pedro Melgueira',
       py_modules=['markov'],
-      install_requires=['numpy==1.11.3'])
+      install_requires=['numpy==1.21.2'])
