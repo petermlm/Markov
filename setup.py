6b845c8242ce511f7562a967b2bec47701eed755
Pedro Melgueira <pedromelgueira@gmail.com>
2021-08-24 12:28:29 +0200

    Adds author email

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@
 from distutils.core import setup
 
 setup(name='Markov',
-      version='1.2',
+      version='1.3',
       description='Markov Text Generation',
       author='Pedro Melgueira',
+      author_email='pm@pedromelgueira.com',
       py_modules=['markov'],
       install_requires=['numpy==1.21.2'])
